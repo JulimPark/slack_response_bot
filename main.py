e078679fca936a3fdf05b9ce40e8ecc5c088a8ac
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-11-28 01:54:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -298,6 +298,45 @@ def message_hello(message, say):
         text=f"Hey there <@{message['user']}>!"
     )
     reruns()
+
+
+def get_day_of_week():
+    weekday_list = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+ 
+    weekday = weekday_list[datetime.datetime.today().weekday()]
+    date = datetime.datetime.today().strftime("%Y년 %m월 %d일")
+    result = '{}({})'.format(date, weekday)
+    return result
+ 
+def get_time():
+    return datetime.datetime.today().strftime("%H시 %M분 %S초")
+ 
+ 
+def get_answer(text):
+    trim_text = text.replace(" ", "")
+ 
+    answer_dict = {
+        '안녕': '안녕하세요. CheckMate입니다.',
+        '요일': ':calendar: 오늘은 {}입니다'.format(get_day_of_week()),
+        '시간': ':clock9: 현재 시간은 {}입니다.'.format(get_time()),
+    }
+ 
+    if trim_text == '' or None:
+        return "알 수 없는 질의입니다. 답변을 드릴 수 없습니다."
+    elif trim_text in answer_dict.keys():
+        return answer_dict[trim_text]
+    else:
+        for key in answer_dict.keys():
+            if key.find(trim_text) != -1:
+                return "연관 단어 [" + key + "]에 대한 답변입니다.\n" + answer_dict[key]
+ 
+        for key in answer_dict.keys():
+            if answer_dict[key].find(text[1:]) != -1:
+                return "질문과 가장 유사한 질문 [" + key + "]에 대한 답변이에요.\n"+ answer_dict[key]
+ 
+    return text + "은(는) 없는 질문입니다."
+
+
 import re
 
 @app.message(re.compile("(hi|hello|hey)"))
@@ -305,6 +344,14 @@ def say_hello_regex(say, context):
     # regular expression matches are inside of context.matches
     greeting = context['matches'][0]
     say(f"{greeting}, how are you?")
+    
+
+@app.message(re.compile("(안녕|요일|시간)"))
+def say_hello_regex(say, context):
+    # regular expression matches are inside of context.matches
+    greeting = context['matches'][0]
+    # say(f"{greeting}, how are you?")
+    say(f"{get_answer(greeting)}")
     
 
 @app.action("button_click")
